b1ed5a5ee01d9226f320f36eaadb77aa20a8f0e7
dmeur <damienmeur@gmail.com>
2023-10-22 12:56:07 +0200

    scaffold

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import { UsersTable } from "./user_management/primary_adapters/components/UsersTable.tsx";
+
+function App() {
+    return (
+        <>
+            <UsersTable/>
+        </>
+    )
+}
+
+export default App
---
+++ src/app/user_management/primary_adapters/components/UsersTable.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+export const UsersTable = (): React.JSX.Element => {
+    return (
+        <>
+        User 1, User 2
+        </>
+    )
+}
+
+
